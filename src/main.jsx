36c5e3a8eaa1f6575b6c4b63cdc1711a5a8e0ce7
randolf <136251983+randolfspages@users.noreply.github.com>
2024-11-04 02:14:14 -0800

    App.jsx and main.jsx rectified

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,37 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createHashRouter, RouterProvider } from 'react-router-dom'
-
+import Portfolio from './pages/Portfolio/Portfolio.jsx';
+import Blogs from './pages/Blogs/Blogs.jsx';
+import Resume from './pages/Resume/Resume.jsx';
+import About from './pages/About/About.jsx';
+import Contact from './pages/Contact/Contact.jsx';
 
 const router = createHashRouter([
-  { path: "/", element: <App /> }
+  { path: "/", element: <App />, 
+    children: [
+      {
+        path: "/",
+        element: <About/>,
+      },
+      {
+        path: "/resume",
+        element: <Resume/>,
+      },
+      {
+        path: "/portfolio",
+        element: <Portfolio/>,
+      },
+      {
+        path: "blogs",
+        element: <Blogs/>
+      },
+      {
+        path: "contact",
+        element: <Contact/>
+      }
+    ]
+   }
 ])
 
 
