6603a69d908838a09a012ca947f2ff056154ca37
randolf <136251983+randolfspages@users.noreply.github.com>
2024-11-04 01:57:35 -0800

    prepared for publish

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
+
+
+const router = createHashRouter([
+  { path: "/", element: <App /> }
+])
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
